4c41a0b2c3abab8cbf77a426d7e92894d902099d
Josiah Sayers <josiah.sayers15@gmail.com>
2019-02-14 08:23:30 -0500

    Add tests for Start1()

---
+++ Day 4/Day4/Day4/Start.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4
+{
+    public class Start
+    {
+        public int Start1(int[] a, int[] b)
+        {
+            int output = 0;
+            return output;
+        }
+    }
+}
---
+++ Day 4/Day4/Day4Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Day4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Day4Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Start test = new Start();
+
+            Assert.AreEqual(2, test.Start1(new int[] { 1, 2, 3 }, new int[] { 1, 2 }));
+            Assert.AreEqual(2, test.Start1(new int[] { 7, 2, 3 }, new int[] { 1 }));
+            Assert.AreEqual(2, test.Start1(new int[] { 1, 2 }, new int[] { 1 }));
+        }
+    }
+}
