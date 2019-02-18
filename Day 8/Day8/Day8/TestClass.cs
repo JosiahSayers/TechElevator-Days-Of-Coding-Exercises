61a0904bbf7136b17b83b86fa2ed539dba55f262
Josiah Sayers <josiah.sayers15@gmail.com>
2019-02-18 08:16:50 -0500

    Add passing code for No23() tests

--- Day 8/Day8/Day8/TestClass.cs
+++ Day 8/Day8/Day8/TestClass.cs
@@ -6,10 +6,15 @@ namespace Day8
 {
     public class TestClass
     {
-
         public bool No23(int[] nums)
         {
-            return false;
+            List<int> numList = new List<int>();
+            numList.AddRange(nums);
+            if (numList.Contains(2) || numList.Contains(3))
+            {
+                return false;
+            }
+            else return true;
         }
     }
 }
