ce2f6d2f850390e8b7ac60053feae773f744008a
Josiah Sayers <josiah.sayers15@gmail.com>
2019-02-20 08:24:03 -0500

    Add tests for More14()

---
+++ Day 10/Day 10/Day10.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_10
+{
+    public class Day10
+    {
+        public bool More14(int[] nums)
+        {
+            int numOnes = 0;
+            int numFours = 0;
+
+            return numOnes > numFours ? true : false;
+        }
+    }
+}
