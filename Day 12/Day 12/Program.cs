3b2d09ee7d125d7ba41b25301180f1831b805fc8
Josiah Sayers <josiah.sayers15@gmail.com>
2019-02-25 08:10:56 -0500

    Add tests

---
+++ Day 12/Day 12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day_12
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int[] MakeLast(int[] nums)
+        {
+            output = new int[0];
+
+            return output;
+        }
+    }
+}
