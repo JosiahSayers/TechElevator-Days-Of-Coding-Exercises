3a82372b80fae6aa7ebcd2fcd8058d35bf13f175
Josiah Sayers <josiah.sayers15@gmail.com>
2019-02-25 19:19:16 -0500

    Add Tests

---
+++ Day 15/Day15/Day15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day15
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool Has23(int[] nums)
+        {
+            bool output = false;
+            return output;
+        }
+    }
+}
---
+++ Day 15/Day15Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Day15;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Day15Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, Program.Has23(new int[] { 2, 5 }));
+            Assert.AreEqual(true, Program.Has23(new int[] { 4, 3 }));
+            Assert.AreEqual(false, Program.Has23(new int[] { 4, 5 }));
+        }
+    }
+}
