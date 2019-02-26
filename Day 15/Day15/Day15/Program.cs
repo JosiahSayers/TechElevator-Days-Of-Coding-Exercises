9a3a8f374c655553bd15743cc45837a962ba22e3
Josiah Sayers <josiah.sayers15@gmail.com>
2019-02-25 19:20:40 -0500

    Add passing code

--- Day 15/Day15/Day15/Program.cs
+++ Day 15/Day15/Day15/Program.cs
@@ -11,8 +11,7 @@ namespace Day15
 
         public static bool Has23(int[] nums)
         {
-            bool output = false;
-            return output;
+            return nums[0] == 2 || nums[1] == 2 || nums[0] == 3 || nums[1] == 3 ? true : false;
         }
     }
 }
